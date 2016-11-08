d8c7bb56af76f86569ee3eaf156fdc166eeb33d7
Hieu Nguyen <hnnguyen45@gmail.com>
2016-11-07 23:21:21 -0800

    test and commit HTTP-echo-server first

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(request, response) {
+    response.json({
+        name: 'Kim Gordon',
+        instrument: 'Bass'
+    });
+});
+
+app.listen(process.env.PORT || 8080, function(){
+	console.log('Server is running at http://localhost:8080');
+});
+
+//Type in node server.js and let it run on gitbash and then type the address http://localhost:8080 on google chrome.
