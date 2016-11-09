4fbfeeccc9aad0bb31616e6b2658a61c6a5f047a
Hieu Nguyen <hnnguyen45@gmail.com>
2016-11-08 23:16:51 -0800

    added new information and also tested out echo http

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+//website to understand this whole concept ... https://debugmode.net/2014/01/14/create-echo-server-in-node-js/
+
 var express = require('express');
 
 var app = express();
@@ -7,6 +9,33 @@ app.get('/', function(request, response) {
         name: 'Kim Gordon',
         instrument: 'Bass'
     });
+});
+
+// When you visit /headers the server should return an object containing the request headers.
+app.get('/headers', function(request, response) {
+  var requestHeaders = request.headers;
+
+  response.json({
+    req: requestHeaders
+  });
+});
+
+// When you visit /headers/:header_name the server should return a string containing the contents of the specified header.
+app.get('/headers/:header_name', function(request, response) {
+  // console.log(request.params);
+  var headerName = request.params.header_name;
+  console.log(headerName);
+  response.json({
+    headerName: request.headers[headerName.toString()]
+    // headerName: request.headers.headerName
+  });
+});
+
+// When you visit /version the server should return a string containing the HTTP version of the request.
+app.get('/version', function (request, response) {
+  response.json({
+    version: request.httpVersion
+  })
 });
 
 app.listen(process.env.PORT || 8080, function(){
